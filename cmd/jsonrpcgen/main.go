40ae9fe9c928067a0289d7fb7377cbf0ec21b485
Blain Smith <rebelgeek@blainsmith.com>
2021-08-16 10:11:24 -0400

    only generate if out dir is specified

--- cmd/jsonrpcgen/main.go
+++ cmd/jsonrpcgen/main.go
@@ -11,6 +11,7 @@ import (
 
 func main() {
 	specFile := flag.String("spec-file", "", "JSON-RPC spec file")
+	browserOutDir := flag.String("browser-out-dir", "", "directory of the generate browser source")
 	goOutDir := flag.String("go-out-dir", "", "directory of the generate Go source")
 	nodeOutDir := flag.String("node-out-dir", "", "directory of the generate Node source")
 	flag.Parse()
@@ -24,7 +25,20 @@ func main() {
 
 	baseSpecFile := "/" + path.Base(*specFile)
 
-	{
+	if *browserOutDir != "" {
+		f, err := os.Create(*browserOutDir + baseSpecFile + ".browser.js")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer f.Close()
+
+		err = spec.GenerateBrowser(f, "./templates", &def)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	if *goOutDir != "" {
 		f, err := os.Create(*goOutDir + baseSpecFile + ".go")
 		if err != nil {
 			log.Fatal(err)
@@ -37,7 +51,7 @@ func main() {
 		}
 	}
 
-	{
+	if *nodeOutDir != "" {
 		f, err := os.Create(*nodeOutDir + baseSpecFile + ".node.js")
 		if err != nil {
 			log.Fatal(err)
